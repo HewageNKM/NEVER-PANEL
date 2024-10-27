47a8548b099ab1fb2e2e4b2947c20ed92a589c8a
Nadun Malwenna <107237482+HewageNKM@users.noreply.github.com>
2024-10-27 14:02:31 +0530

    reduce id length

--- utils/genarateIds.ts
+++ utils/genarateIds.ts
@@ -1,9 +1,9 @@
 export const generateId = (type:string,name:string) => {
     switch (type) {
         case 'item':
-            return `${name.trim()}-${window.crypto.randomUUID().replace("-","").substring(0, 10)}`.toUpperCase();
+            return `${name.trim()}-${window.crypto.randomUUID().replace("-","").substring(0, 5)}`.toUpperCase();
          case 'variant':
-             return `VA-${window.crypto.randomUUID().replace("-","").substring(0, 10)}`
+             return `VA-${window.crypto.randomUUID().replace("-","").substring(0, 5)}`
         default:
             return ""
     }
