50dbcb0e25e576852ad0b95f309f64ad6ee0d65b
Nadun Malwenna <107237482+HewageNKM@users.noreply.github.com>
2024-10-28 22:20:10 +0530

    Remodel inventory page

--- app/adminPanel/inventory/components/InventoryManage.tsx
+++ app/adminPanel/inventory/components/InventoryManage.tsx
@@ -1,220 +1,51 @@
 "use client"
 import React, {useEffect, useState} from 'react';
 import ItemCard from "@/app/adminPanel/inventory/components/ItemCard";
-import Loading from "@/app/adminPanel/components/Loading";
 import {AnimatePresence} from "framer-motion";
 import ItemForm from "@/app/adminPanel/inventory/components/ItemForm";
 import {AppDispatch} from "@/lib/store";
 import {useDispatch} from "react-redux";
 import {Item} from "@/interfaces";
-import {generateId} from "@/utils/genarateIds";
-import {
-    deleteFilesFromStorage,
-    deleteInventoryItem,
-    filterInventoryByBrands,
-    getInventory,
-    getToken,
-    uploadImages
-} from "@/firebase/firebaseClient";
-import {hideLoader, showLoader} from "@/lib/pageLoaderSlice/pageLoaderSlice";
-import {showToast} from "@/lib/toastSlice/toastSlice";
 import {brands} from "@/constant";
 import {IoAdd, IoSearch} from "react-icons/io5";
 import EmptyState from "@/components/EmptyState";
 import axios from "axios";
-import {Timestamp} from "@firebase/firestore";
+import {getCurrentUser} from "@/firebase/firebaseClient";
 
-const InventoryManage = () => {
-    const [loadItemTable, setLoadItemTable] = useState(true)
-    const [addForm, setAddForm] = useState(false)
+const InventoryManage = ({it}: { it: Item[] }) => {
     const dispatch: AppDispatch = useDispatch();
-    const [inventoryList, setInventoryList] = useState([] as Item[])
-    const [refreshItemTable, setRefreshItemTable] = useState(false)
 
-    // Add Item Form
-    const [id, setId] = useState('');
-    const [manufacture, setManufacture] = useState('none')
-    const [name, setName] = useState('')
-    const [buyingPrice, setBuyingPrice] = useState("")
-    const [sellingPrice, setSellingPrice] = useState("")
-    const [discount, setDiscount] = useState("")
-    const [updateState, setUpdateState] = useState(false)
-    const [type, setType] = useState("none")
-    const [brand, setBrand] = useState("")
-    const [thumbnail, setThumbnail] = useState<{ file: null | File, url: string | null }>({file: null, url: null})
-    const [createdAt, setCreatedAt] = useState<Timestamp>(Timestamp.now)
-    const [updatedAt, setUpdatedAt] = useState<Timestamp>(Timestamp.now)
+    const [page, setPage] = useState(1)
+    const [size, setSize] = useState(20)
 
-    const onAddItemFormSubmit = async (evt: any) => {
-        evt.preventDefault();
+    const [showForm, setShowForm] = useState(false)
+    const [items, setItems] = useState(it)
 
-        if (manufacture === "none") {
-            showMessage("Please, select the manufacture", "Warning")
-            return;
-        }
-
-        if (type === "none") {
-            showMessage("Please, select the type", "Warning")
-            return;
-        }
-
-        if (id.trim().length === 0) {
-            if (thumbnail.file === null) {
-                showMessage("Please, upload a thumbnail", "Warning")
-                return
-            }
-            const genId: string = generateId("item", manufacture);
-            const url = await uploadImages([thumbnail], `inventory/${genId.toLowerCase()}/`);
-
-            const item: Item = {
-                thumbnail: url[0],
-                variants: [],
-                type: type.toLowerCase(),
-                brand: brand.toLowerCase(),
-                buyingPrice: Number.parseInt(buyingPrice),
-                discount: Number.parseInt(discount),
-                itemId: genId.toLowerCase(),
-                manufacturer: manufacture.toLowerCase(),
-                name: name,
-                sellingPrice: Number.parseInt(sellingPrice),
-            }
-
-            await saveItem(item, "Item added successfully","save")
-
-        } else if (id.trim().length > 0) {
-            const i = inventoryList.find((item) => item.itemId === id);
-
-            const item: Item = {
-                brand: brand.toLowerCase(),
-                type: type.toLowerCase(),
-                buyingPrice: Number.parseInt(buyingPrice),
-                discount: Number.parseInt(discount),
-                itemId: id.toLowerCase(),
-                manufacturer: manufacture.toLowerCase(),
-                name: name,
-                sellingPrice: Number.parseInt(sellingPrice),
-                thumbnail: i?.thumbnail || "",
-                variants: i?.variants || [],
-                createdAt: createdAt,
-                updatedAt: updatedAt
-            }
-            // Update Item and setUpdateState to false so when add manufacture name can be edited
-            await saveItem(item, "Item updated successfully","update")
-            setUpdateState(false)
-        }
-
-    }
-    const saveItem = async (item: Item, message: string,type:string) => {
-        try {
-            dispatch(showLoader())
-            const token = await getToken()
-            if(type === "update"){
-                const res = await axios({
-                    method: "PUT",
-                    url: "/api/inventory/"+item.itemId,
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                    data: JSON.stringify(item)
-                });
-                if(res.status != 200){
-                    showMessage("Error updating item", "Error")
-                    return;
-                }
-
-                setAddForm(false)
-                setRefreshItemTable(prevState => !prevState)
-                showMessage(message, "Success")
-                clearAddFormField();
-            }else if(type === "save"){
-                const res = await axios({
-                    method: "POST",
-                    url: "/api/inventory",
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                    data: JSON.stringify(item)
-                });
-
-                if(res.status != 200){
-                    showMessage("Error saving item", "Error")
-                    return;
-                }
-
-                setAddForm(false)
-                setRefreshItemTable(prevState => !prevState)
-                showMessage(message, "Success")
-                clearAddFormField();
-            }
-        } catch (e: any) {
-            console.log(e)
-            showMessage(e.message, "Error")
-        } finally {
-            dispatch(hideLoader())
-        }
-    }
-
-    // Delete Item
-    const deleteItemFromInventory = async (id: string) => {
-        const response = confirm(`Are you sure you want to delete this item with ID ${id}?`);
-        if (response) {
-            try {
-                await deleteFilesFromStorage(`inventory/${id}`)
-                await deleteInventoryItem(id);
-                setRefreshItemTable(prevState => !prevState)
-                showMessage("Item updated successfully", "Success")
-                return;
-            } catch (e: any) {
-                showMessage(e.message, "Error")
-            }
-        }
-    }
-
-    //Filter InventoryManage by Brands
-    const filterInventory = async (brands: string) => {
-        if (brands === "all") {
-            setRefreshItemTable(prevState => !prevState)
-        }
-        try {
-            const items = await filterInventoryByBrands(brands)
-            setInventoryList(items)
-        } catch (e: any) {
-            showMessage(e.message, "Error")
-        }
-    }
+    const [selectedItem, setSelectedItem] = useState({} as Item)
 
     useEffect(() => {
-        setLoadItemTable(true)
-        getInventory().then((items) => {
-            setInventoryList(items)
-        }).catch((e) => {
-            showMessage(e.message, "Error")
-        }).finally(() => setLoadItemTable(false))
-    }, [refreshItemTable, dispatch])
+        fetchItems()
+    }, [dispatch, selectedItem])
 
-    const clearAddFormField = () => {
-        setManufacture("none")
-        setName("")
-        setBuyingPrice("")
-        setSellingPrice("")
-        setDiscount("")
-        setId("")
-        setBrand("")
-        setType("none")
-        setThumbnail({
-            file: null,
-            url: null
-        })
+    const fetchItems = async () => {
+        const uid = getCurrentUser()?.uid;
+        const token = await getCurrentUser()?.getIdToken()
+
+        console.log("Fetching")
+        const res = await axios({
+            method: 'GET',
+            url: `/api/inventory?page=${page}&size=${size}&uid=${uid}`,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+
+        setItems(res.data)
     }
 
-    const showMessage = (message: string, type: string) => {
-        dispatch(showToast({message: message, type: type, showToast: true}))
-        setTimeout(() => dispatch(showToast({message: "", type: "", showToast: false})), 3000);
-        return;
-    }
     return (
         <>
-            <div className="mt-2 flex justify-between flex-wrap gap-5 flex-col lg:flex-row">
+            <div className="mt-2 px-4 py-4  flex justify-between flex-wrap gap-5 flex-col lg:flex-row">
                 <div className="flex flex-col gap-1">
                     <label className="flex flex-col gap-1 mb-2">
                         <span className="font-medium text-lg lg:text-xl">Search</span>
@@ -232,7 +63,6 @@ const InventoryManage = () => {
                     <label className="flex flex-col mt-2">
                         <span className="font-bold text-lg">Brands</span>
                         <select defaultValue="all"
-                                onChange={(txt) => filterInventory(txt.target.value)}
                                 className="w-full md:w-[15rem] p-1 border-2 text-black rounded font-medium">
                             {brands.map((brand, index) => (
                                 <option key={index} value={brand.value}>{brand.name}</option>
@@ -242,57 +72,27 @@ const InventoryManage = () => {
                     </label>
                 </div>
                 <div className="flex justify-center items-center">
-                    <button onClick={() => setAddForm(true)}
+                    <button onClick={() => setShowForm(true)}
                             className="bg-primary-100 text-white flex flex-row justify-center items-center h-[2.8rem] px-3 py-1 rounded hover:bg-primary-200">
                         <IoAdd size={30}/>
                         Add Item
                     </button>
                 </div>
             </div>
-            <div className="w-full mt-10 flex flex-row flex-wrap gap-10 lg:gap-20 justify-center items-center">
-                {!loadItemTable && inventoryList.map((item, index) => (
+            <div className="w-full my-10 flex flex-row flex-wrap gap-10 lg:gap-20 justify-center items-center">
+                {items.map((item, index) => (
                     <ItemCard item={item} key={index} onEdit={() => {
-                        setType(item.type)
-                        setBrand(item.brand)
-                        setId(item.itemId)
-                        setManufacture(item.manufacturer)
-                        setName(item.name)
-                        setBuyingPrice(item.buyingPrice.toString())
-                        setSellingPrice(item.sellingPrice.toString())
-                        setDiscount(item.discount.toString())
-                        setThumbnail({
-                            file: null,
-                            url: item.thumbnail
-                        })
-                        setCreatedAt(item.createdAt)
-                        setUpdatedAt(item.updatedAt)
-                        setUpdateState(true)
-                        setAddForm(true)
-                    }} onDelete={() => deleteItemFromInventory(item.itemId)}/>
+                        setSelectedItem(item)
+                        setShowForm(true)
+                    }} onDelete={() => {
+                    }}/>
                 ))}
-                {loadItemTable && <Loading/>}
-                {inventoryList.length === 0 && <EmptyState title={"Not Found"} subtitle={"Add Items"}/>}
+                {items.length === 0 && <EmptyState title={"Not Found"} subtitle={"Add Items"}/>}
             </div>
             <AnimatePresence>
-                {addForm && (
-                    <ItemForm brand={brand} setBrand={setBrand}
-                              updateState={updateState}
-                              setUpdateState={setUpdateState}
-                              discount={discount}
-                              sellingPrice={sellingPrice}
-                              buyingPrice={buyingPrice}
-                              name={name}
-                              manufacture={manufacture}
-                              setAddForm={setAddForm}
-                              setDiscount={setDiscount}
-                              setBuyingPrice={setBuyingPrice}
-                              setName={setName}
-                              setManufacture={setManufacture}
-                              setSellingPrice={setSellingPrice}
-                              id={id} setId={setId}
-                              setThumbnail={setThumbnail}
-                              thumbnail={thumbnail}
-                              onSubmit={onAddItemFormSubmit} setType={setType} type={type}/>)}
+                {showForm && (
+                    <ItemForm selectedItem={selectedItem} setSelectedItem={setSelectedItem}
+                              setShowForm={setShowForm}/>)}
             </AnimatePresence>
         </>
     );
--- app/api/inventory/[itemId]/route.ts
+++ app/api/inventory/[itemId]/route.ts
@@ -1,15 +1,24 @@
-import {saveToInventory, updateItem, verifyIdToken} from "@/firebase/firebaseAdmin";
+import {updateItem, verifyIdToken} from "@/firebase/firebaseAdmin";
 import {NextResponse} from "next/server";
 
 export const PUT = async (req: Request) => {
     try {
         // Verify the ID token
-        await verifyIdToken(req);
-        let body = await req.json();
+        const res = await verifyIdToken(req);
+        if (res.status == 401) {
+            return NextResponse.json({message: 'Unauthorized'}, {status: 401})
+        }
+        const uid = new URL(req.url).searchParams.get("uid");
+
+        if(!uid){
+            return NextResponse.json("UID Not Provided", {status:401})
+        }
+
+        const body = await req.json();
         await updateItem(body);
-        return NextResponse.json({ message: 'Item saved successfully' },{status: 200});
+        return NextResponse.json({message: 'Item saved successfully'}, {status: 200});
     } catch (error: any) {
         console.error(error);
-        return NextResponse.json({ message: 'Error fetching orders', error: error.message }, { status: 500 });
+        return NextResponse.json({message:error.message}, {status: 500});
     }
 };
