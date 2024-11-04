77246bf57624dddaf2f668d5ffc96bb59ac4ebfa
Nadun Malwenna <107237482+HewageNKM@users.noreply.github.com>
2024-11-04 10:20:56 +0530

    redesign not-found.tsx

--- app/dashboard/typography/page.tsx
+++ app/dashboard/typography/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { Typography, Grid, CardContent } from '@mui/material';
-import PageContainer from '../../components/container/PageContainer';
-import DashboardCard from '../../components/shared/DashboardCard';
-import BlankCard from '../../components/shared/BlankCard';
+import PageContainer from '../components/container/PageContainer';
+import DashboardCard from '../components/shared/DashboardCard';
+import BlankCard from '../components/shared/BlankCard';
 
 
 const TypographyPage = () => {
