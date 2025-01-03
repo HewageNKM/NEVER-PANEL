100c769284046dd0d3ec9c1210f4e4fe89775466
Nadun Malwenna <107237482+HewageNKM@users.noreply.github.com>
2025-01-03 22:21:05 +0530

    Orders can now search

--- app/dashboard/orders/components/OrdersHeader.tsx
+++ app/dashboard/orders/components/OrdersHeader.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, FormControl, IconButton, InputLabel, MenuItem, Select, Stack, TextField} from '@mui/material';
 import Typography from "@mui/material/Typography";
 import {IoRefreshCircle} from "react-icons/io5";
 import {useAppDispatch, useAppSelector} from "@/lib/hooks";
-import {getOrders} from "@/lib/ordersSlice/ordersSlice";
+import {getOrders, setOrders} from "@/lib/ordersSlice/ordersSlice";
+import {getAlgoliaClient} from "@/lib/middleware";
 
 const OrdersHeader = () => {
     const dispatch = useAppDispatch();
     const {selectedPage, size} = useAppSelector(state => state.ordersSlice);
+
+    const onSearch = async (evt) => {
+        try {
+            evt.preventDefault();
+            const search = evt.target.search.value;
+            const client = getAlgoliaClient();
+            const searchResults = await client.search({
+                requests: [{indexName: "orders_index", query: search}]
+            });
+            console.log(searchResults.results[0].hits)
+            dispatch(setOrders(searchResults.results[0].hits))
+        }catch (e) {
+            console.error(e)
+        }
+    }
     return (
         <Stack direction="column" spacing={2} alignItems="start" justifyContent="space-between" p={2}>
             <Stack>
@@ -15,7 +31,7 @@ const OrdersHeader = () => {
                     Options
                 </Typography>
             </Stack>
-            <Stack direction="row" spacing={5} alignItems="center" justifyContent="space-between" py={2}>
+            <Stack direction="row" spacing={5} alignItems="center" justifyContent="space-between" py={2} flexWrap={"wrap"}>
                 <Stack direction="row" spacing={2} alignItems="center">
                     {/* Filter Dropdown */}
                     <FormControl variant="outlined" size="small">
@@ -52,16 +68,21 @@ const OrdersHeader = () => {
                         </Select>
                     </FormControl>
                 </Stack>
-                <Stack direction="row" spacing={1} alignItems="center">
+                <Stack direction="row" spacing={1} alignItems="center" flexWrap={"wrap"}>
                     {/* Search TextField */}
-                    <TextField
-                        variant="outlined"
-                        size="small"
-                        placeholder="Search orders..."
-                    />
-                    <Button variant="contained" color="primary">
-                        Search
-                    </Button>
+                    <form onSubmit={onSearch}>
+                        <Stack gap={2} display={"flex"} direction={"row"} flexWrap={"wrap"}>
+                            <TextField
+                                variant="outlined"
+                                size="small"
+                                placeholder="Search orders..."
+                                name={"search"}
+                            />
+                            <Button type={"submit"} variant="contained" color="primary">
+                                Search
+                            </Button>
+                        </Stack>
+                    </form>
                     <IconButton onClick={() => dispatch(getOrders({size, page: selectedPage}))}>
                         <IoRefreshCircle size={30}/>
                     </IconButton>
