21584fd0c4b17f134f2d8ab8533d05cb47c3542c
Ethan Blake <107237482+EthanBlake00@users.noreply.github.com>
2025-02-17 22:28:52 +0530

    improve PopularItemCard.tsx design

--- app/dashboard/components/dashboard/PopularItemCard.tsx
+++ app/dashboard/components/dashboard/PopularItemCard.tsx
@@ -37,12 +37,13 @@ const PopularItemCard = ({ item }: { item: PopularItem }) => {
                         variant="body2"
                         sx={{ color: "text.secondary", fontSize: "0.8rem", mt: 0.5 }}
                     >
-                        LKR {item.item.sellingPrice}
+                        LKR {Math.round(item.item.sellingPrice - (item.item.sellingPrice * item.item.discount / 100)).toFixed(2)}
                     </Typography>
                     <Box
                         sx={{
                             background: "#F5F5F5",
                             borderRadius: 1,
+                            fontWeight: 600,
                             display: "inline-block",
                             px: 1,
                             py: 0.3,
@@ -53,6 +54,31 @@ const PopularItemCard = ({ item }: { item: PopularItem }) => {
                         Sold: {item.soldCount}
                     </Box>
                 </CardContent>
+                <Box
+                    sx={{
+                        backgroundColor: "primary.main",
+                        borderRadius: "0 0 8px 8px",
+                        p: .8,
+                        mt: -1,
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                    }}
+                >
+                    <Typography
+                        variant="body2"
+                        sx={{
+                            textTransform: "capitalize",
+                            color: "white",
+                            fontSize: "0.8rem",
+                            fontWeight: 600,
+                            mt: 0.5,
+                            textAlign: "center",
+                        }}
+                    >
+                        {item.item.manufacturer}
+                    </Typography>
+                </Box>
             </CardActionArea>
         </Card>
     );
